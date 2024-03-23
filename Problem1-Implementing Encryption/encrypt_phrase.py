a9cdc35308ef063e7a8ad9376d7583405c3c33f4
Nuwan Udara <nuwanudara36@gmail.com>
2024-03-23 21:12:31 +0530

    Added new padding

--- Problem1-Implementing Encryption/encrypt_phrase.py
+++ Problem1-Implementing Encryption/encrypt_phrase.py
@@ -23,7 +23,6 @@ def encrypt_phrase(phrase):
 
   # Encrypt the phrase.
   encrypted_phrase = ciper.encrypt(phrase)
-  
 
   # Encode the encrypted phrase to base64"
   encrypted_phrase = base64.b64encode(encrypted_phrase)
@@ -34,18 +33,16 @@ def encrypt_phrase(phrase):
 
 
 def pad(data, block_size):
-  """ Function pads a string (Phrase entered by user) to a multiple of the specified block size to meet AES requirments.
-  Inouts:
-    data: The phrase to be pad.
-    block_size: The block size to be pad to.
-  Outpur:
-    The padded string (Phrase).
-  """
-  print(data,block_size)
-  padding_len = block_size - len(data) % block_size
-  padding = chr(padding_len) * padding_len  # PKCS#7 padding methology
-  padding = padding.encode('utf-8')
-  return data + padding
+    """ Function pads a string (Phrase entered by user) to a multiple of the specified block size to meet AES requirements.
+    Inputs:
+        data: The phrase to be padded.
+        block_size: The block size to be padded to.
+    Output:
+        The padded string (Phrase).
+    """
+    padding_len = block_size - (len(data) % block_size)
+    padding = bytes([padding_len] * padding_len) # PKCS#7 padding technuique is used
+    return data + padding
 
 if __name__ == "__main__":
   # Get the phrase to encrypt from the user.
@@ -53,6 +50,7 @@ if __name__ == "__main__":
 
   # Encrypt the phrase.
   output,key = encrypt_phrase(phrase)
-
+  print("Before decode",output)
+  encrypted_phrase_str = output.decode('utf-8')
   # Print the encrypted phrase, passphrase, and the key.
-  print("Phase: ", phrase, "Passphrase: ", output, "Key: ", key)
+  print("Phase:", phrase, "Passphrase:", encrypted_phrase_str, "Key:", key)
